a69079bab4790fde8d43881e816b035afba52ef1
Pierre Vermeulen <pierre.vermeulen@espci.org>
2020-12-04 15:09:42 +0100

    app.py v2 sidebar

--- app.py
+++ app.py
@@ -3,22 +3,29 @@ import datetime
 import requests
 import numpy as np
 import pandas as pd
+import pydeck as pdk
 
 '''
 # TaxiFareModel Prediction
 '''
 
-d = st.date_input(
-    "Pickup date", datetime.datetime.now())
-t = st.time_input('Pickup time', datetime.time(12,0))
+init_lat = 40.783
+init_long = -73.966
 
+st.sidebar.markdown("<font color='red'> Pick-up</font>", unsafe_allow_html=True)
+d = st.sidebar.date_input("Pickup date", datetime.datetime.now())
+t = st.sidebar.time_input('Pickup time', datetime.time(12,0))
 pickup_datetime = datetime.datetime.combine(d, t)
-pass_number = st.number_input('Passenger number', min_value=1, max_value=20)
-pu_lat = st.number_input('Pickup latitude', min_value=38.0, max_value=42.0, value = 40.783)
-pu_long = st.number_input('Pickup longitude', min_value=-75.0, max_value=-72.0, value = -73.966)
-
-do_lat = st.number_input('Dropoff latitude', min_value=38.0, max_value=42.0, value = 40.783)
-do_long = st.number_input('Dropoff longitude', min_value=-75.0, max_value=-72.0, value = -73.966)
+pass_number = st.sidebar.number_input('Passenger number', min_value=1, max_value=20)
+pu_lat = st.sidebar.number_input('Pickup latitude', min_value=38.0, max_value=42.0, \
+    value = init_lat, format="%.5f")
+pu_long = st.sidebar.number_input('Pickup longitude', min_value=-75.0, max_value=-72.0, \
+    value = init_long, format="%.5f")
+st.sidebar.markdown("<font color='blue'> Drop-off</font>", unsafe_allow_html=True)
+do_lat = st.sidebar.number_input('Dropoff latitude', min_value=38.0, max_value=42.0, \
+    value = init_lat, format="%.5f")
+do_long = st.sidebar.number_input('Dropoff longitude', min_value=-75.0, max_value=-72.0, \
+    value = init_long, format="%.5f")
 
 url = 'https://taxifare.lewagon.ai/predict_fare/'
 params = {'key': 'key',
@@ -33,7 +40,34 @@ if st.button('Fare prediction'):
     response = requests.get(url, params = params)
     pred = response.json()
     st.write(f"Predicted fare = {pred['prediction']:.2f}$")
-    df = pd.DataFrame(
-            np.array([[pu_lat, pu_long], [do_lat, do_long]]),
+
+df_pu = pd.DataFrame(np.array([[pu_lat, pu_long]]),
             columns=['lat', 'lon'])
-    st.map(df)
+df_do = pd.DataFrame(np.array([[do_lat, do_long]]),
+            columns=['lat', 'lon'])
+#st.map(df)
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=init_lat,
+        longitude=init_long,
+        zoom = 12,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=df_pu,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=100,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=df_do,
+            get_position='[lon, lat]',
+            get_color='[0, 30, 200, 160]',
+            get_radius=100,
+        )
+    ],
+))
